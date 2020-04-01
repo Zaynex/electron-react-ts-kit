7ffe332e56bf5987b4c17b04929e3e29bad02da3
zaynex <zaynex@126.com>
2020-04-01 17:03:34 +0800

    test: hook issue

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,17 +1,54 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useCallback, useState } from 'react'
+import { useToggle } from 'react-use'
+
+const useToggleFromLocalFile = (initialValue: boolean): [boolean, (nextValue?: any) => void] => {
+  const [value, setValue] = useState<boolean>(initialValue)
+
+  const toggle = useCallback(
+    (nextValue?: any) => {
+      if (typeof nextValue === 'boolean') {
+        setValue(nextValue)
+      } else {
+        setValue(currentValue => !currentValue)
+      }
+    },
+    [setValue],
+  )
+
+  return [value, toggle]
+}
 
 export interface Props {
   name: string
 }
 
-export default class App extends React.Component<Props, object> {
-  render(): ReactElement {
-    const { name } = this.props
+export default function App({ name }: Props): ReactElement {
+  return (
+    <div>
+      <div>Hi {name}</div>
+      <Fail />
+      <OK />
+    </div>
+  )
+}
+function Fail(): ReactElement {
+  // notice , it's failed if import from react-use
+  const [on, toggle] = useToggle(false)
+  return (
+    <div>
+      <div>{on ? 'on' : 'close'}</div>
+      <button onClick={toggle}>toggle</button>
+    </div>
+  )
+}
 
-    return (
-      <div className="hello">
-        <div className="greeting">Hello {name}</div>
-      </div>
-    )
-  }
+function OK(): ReactElement {
+  // it's ok if load from local
+  const [on, toggle] = useToggleFromLocalFile(false)
+  return (
+    <div>
+      <div>{on ? 'on' : 'close'}</div>
+      <button onClick={toggle}>toggle</button>
+    </div>
+  )
 }
